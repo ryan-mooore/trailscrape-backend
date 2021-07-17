60afcbfc33e65bc62a670b1596f14c40e13df497
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-07-17 22:34:01 +1200

    oops2

--- src/update_trail_status.py
+++ src/update_trail_status.py
@@ -16,20 +16,21 @@ if __name__ == "__main__":
             for park_ID, park in region["parks"].items():
                 try:
                     if park_ID != argv[1]: continue
-                finally:
-                    new_status = {
-                        "scrapeError": False
+                except IndexError:
+                    pass
+                new_status = {
+                    "scrapeError": False
+                }
+                try:
+                    new_status["status"] = importlib.import_module("methods." + re.sub(r"(?<!^)(?=[A-Z])", "_", park["method"]).lower()).main(park_ID, park)
+                    new_status["scrapeTime"] = datetime.utcnow()
+                    new_status["weather"] = {
+                        "temp": weather.get_temp(**park["coords"]),
+                        "conditions": weather.get_conditions(**park["coords"])
                     }
-                    try:
-                        new_status["status"] = importlib.import_module("methods." + re.sub(r"(?<!^)(?=[A-Z])", "_", park["method"]).lower()).main(park_ID, park)
-                        new_status["scrapeTime"] = datetime.utcnow()
-                        new_status["weather"] = {
-                            "temp": weather.get_temp(**park["coords"]),
-                            "conditions": weather.get_conditions(**park["coords"])
-                        }
-                    except Exception as e:
-                        print(f"Error while scraping {park['name']}: {e}")
-                        status["activities"][activity][region_ID]["parks"][park_ID]["scrapeError"] = True
-                    else:
-                        status["activities"][activity][region_ID]["parks"][park_ID] = new_status
+                except Exception as e:
+                    print(f"Error while scraping {park['name']}: {e}")
+                    status["activities"][activity][region_ID]["parks"][park_ID]["scrapeError"] = True
+                else:
+                    status["activities"][activity][region_ID]["parks"][park_ID] = new_status
     db.status.update_one({"_id": status["_id"]}, {"$set": status})
