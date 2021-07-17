8d1c9f34e5dc3a2787e3c60927cee42b79353329
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-07-17 18:23:50 +1200

    added more weather info

--- src/update_trail_status.py
+++ src/update_trail_status.py
@@ -3,8 +3,17 @@ from documents.documents import db
 from methods import scrape_park_and_get_trails_from_trailforks, api, copy_from_trailforks, scrape_trails, scrape_status_and_get_grade_from_trailforks
 from datetime import datetime
 import logging
+from other import weather
 
 logging.basicConfig(level=20)
+
+method_map = {
+    "scrapeTrails": scrape_trails,
+    "scrapeParkAndGetTrailsFromTrailforks": scrape_park_and_get_trails_from_trailforks,
+    "scrapeStatusAndGetGradeFromTrailforks": scrape_status_and_get_grade_from_trailforks,
+    "copyFromTrailforks": copy_from_trailforks,
+    "api": api
+}
 
 if __name__ == "__main__":
     regions = db.regions.find_one()
@@ -16,18 +25,15 @@ if __name__ == "__main__":
                     "scrapeError": False
                 }
                 try:
-                    for k, v in {
-                        "scrapeTrails": scrape_trails,
-                        "scrapeParkAndGetTrailsFromTrailforks": scrape_park_and_get_trails_from_trailforks,
-                        "scrapeStatusAndGetGradeFromTrailforks": scrape_status_and_get_grade_from_trailforks,
-                        "copyFromTrailforks": copy_from_trailforks,
-                        "api": api
-                    }[park["method"]].main(park_ID, park).items():
-                        new_status[k] = v
-                        new_status["scrapeTime"] = datetime.utcnow()
+                    new_status["status"] = method_map[park["method"]].main(park_ID, park)
+                    new_status["scrapeTime"] = datetime.utcnow()
+                    new_status["weather"] = {
+                        "temp": weather.get_temp(**park["coords"]),
+                        "conditions": weather.get_conditions(**park["coords"])
+                    }
                 except Exception as e:
                     print(f"Error while scraping {park['name']}: {e}")
-                    status["activities"][activity][region_ID]["parks"][park_ID].scrapeError = True
+                    status["activities"][activity][region_ID]["parks"][park_ID]["scrapeError"] = True
                 else:
                     status["activities"][activity][region_ID]["parks"][park_ID] = new_status
     db.status.update_one({"_id": status["_id"]}, {"$set": status})
