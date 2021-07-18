408ff118aa335f4f5809eb4bd6b6000fca30cff1
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-07-18 23:35:12 +1200

    added release/postbuild jobs

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo ----- update regions -----
+pipenv run python src/update_regions.py
+echo ----- update trailforks regions -----
+pipenv run python src/update_trailforks_regions.py
+echo ----- create trail status template -----
+pipenv run python src/create_trail_status_template.py
+echo -----  update trail status -----
+pipenv run python src/update_trail_status.py
