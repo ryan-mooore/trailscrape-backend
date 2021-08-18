7a2b089c5a17157ee3fcd0cfa82ffd0b552817ee
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-08-18 21:29:12 +1200

    rename api key

--- src/other/weather.py
+++ src/other/weather.py
@@ -4,11 +4,11 @@ from os import environ
 def get_temp(lat: float, lon: float) -> int:
     return round(
         requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={environ['YR_API_KEY']}"
+            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={environ['OWM_API_KEY']}"
         ).json()["main"]["temp"] - 273.15
     )
 
 def get_conditions(lat: float, lon: float) -> dict:
     return requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={environ['YR_API_KEY']}"
+           f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={environ['OWM_API_KEY']}"
     ).json()["weather"][0]
