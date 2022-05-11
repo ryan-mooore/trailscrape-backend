ad87e561fb8ee7ef1261c7c411986048b2b4687a
Ryan Moore <kiwi.ryanmoore@gmail.com>
2022-05-11 16:18:36 +1200

    correctly infer lift status

--- src/methods/status/lift/infer_from_trails.py
+++ src/methods/status/lift/infer_from_trails.py
@@ -3,4 +3,4 @@ from helpers.setup import log
 
 def main(**kwargs: Any) -> bool:
     log.info("Inferring lift status from trail status...")
-    return True in [trail["isOpen"] for trail in kwargs["status"]["trails"]]
+    return True in [trail["isOpen"] for trail in kwargs["status"]["trails"]] and kwargs["status"]["park"]
